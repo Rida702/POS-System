1eed9c438b5cee6e3806cca6792931e2359a62fc
Rida Shabbir <107110619+Rida702@users.noreply.github.com>
2022-08-03 13:38:19 -0700

    Update wsgi.py

--- POS System/wsgi.py
+++ POS System/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'POS_1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'POS_System.settings')
 
 application = get_wsgi_application()
